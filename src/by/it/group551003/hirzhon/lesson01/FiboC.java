49bd7d892f04970cb41bfc50786d0d5a2243334b
MaksimHirzhon <maksimhirzhon@gmail.com>
2026-03-07 02:10:25 +0300

    Lesson01 A B C

--- src/by/it/group551003/hirzhon/lesson01/FiboC.java
+++ src/by/it/group551003/hirzhon/lesson01/FiboC.java
@@ -21,10 +21,35 @@ public class FiboC {
         return System.currentTimeMillis() - startTime;
     }
 
+    static long[][] matrixMultiply(long[][] a, long[][] b, int m) {
+        long[][] r = new long[2][2];
+        r[0][0] = (a[0][0] * b[0][0] + a[0][1] * b[1][0]) % m;
+        r[0][1] = (a[0][0] * b[0][1] + a[0][1] * b[1][1]) % m;
+        r[1][0] = (a[1][0] * b[0][0] + a[1][1] * b[1][0]) % m;
+        r[1][1] = (a[1][0] * b[0][1] + a[1][1] * b[1][1]) % m;
+        return r;
+    }
+
+    private long[][] exponentation(long[][] mtx, long n, int m) {
+        long[][] r ={{1, 0}, {0, 1}};
+
+        while (n > 0) {
+            if (n % 2 == 1)
+                r = matrixMultiply(r, mtx, m);
+            mtx = matrixMultiply(mtx, mtx, m);
+            n = n / 2;
+        }
+        return r;
+    }
+
     long fasterC(long n, int m) {
-        //Интуитивно найти решение не всегда просто и
-        //возможно потребуется дополнительный поиск информации
-        return -1L;
+        long[][] matrix = { {1, 1}, {1, 0} };
+        if (n == 0) return 0L;
+        if (n == 1) return 1 % m;
+        if (n == 2) return 1 % m;
+
+        long[][] p = exponentation(matrix, n - 1, m);
+        return p[0][0] % m;
     }
 
 
